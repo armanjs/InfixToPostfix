4d69c5c56dab484c1b1c90a5127ff450df121440
Arman Sadeghi <arman1998@gmail.com>
2020-11-11 16:27:40 -0800

    added mathExp.cpp for extra credit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "stackArray.h"
 #include "InfixToPostfix.h"
+#include "mathExp.h"
 #include <stack>
 
 
@@ -15,7 +16,7 @@ int main() {
     //cout << stack.top() << endl;
     stack.pop();
     cout << stack.top() << endl;
-    */
+
     cout << "enter a infix statement to be converted to postfix: ";
     string infix = "";
     cin >> infix;
@@ -23,7 +24,12 @@ int main() {
     inToPost.getInfix(infix);
     inToPost.showInfix();
     inToPost.convertToPostFix();
-    inToPost.showInfix();
     inToPost.showPostfix();
+    */
+    char str[] = "3+4";
+    //mathExp exp1 (str);
+    int length = sizeof(str)/sizeof(char);
+    cout << length;
+
     return 0;
 }
